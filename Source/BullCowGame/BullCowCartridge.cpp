c8c52265e49adf06654dcd25411e01e4b2c33d82
Robert Lafferty <rclafferty@huskers.unl.edu>
2021-08-22 17:34:43 -0400

    Updated isIsogram() code to use a TMap.

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -6,46 +6,167 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     Super::BeginPlay();
 
     PrintLine(TEXT("Welcome to Bull Cows!"));
-    PrintLine(TEXT("Press enter to continue...\n"));
+    PrintLine(TEXT("Guess the hidden isogram (no repeating"
+                   "\nletters) within the allotted amount"
+                   "\nof guesses."));
+    PrintLine(TEXT("Press tab to start typing."));
+    PrintLine(TEXT("Press enter to continue..."));
 
-    // Set the hidden word
-    HiddenWord = TEXT("napkin");
+    bGameOver = true;
 
-    // Set lives
-    Lives = HiddenWord.Len();
-
-    // Prompt player for a guess
+    /*****************************************/
+    /* Initialize the list of possible words */
+    /*****************************************/
+    PossibleWords.push_back(TEXT("heat"));
+    PossibleWords.push_back(TEXT("work"));
+    PossibleWords.push_back(TEXT("employ"));
+    PossibleWords.push_back(TEXT("shock"));
+    PossibleWords.push_back(TEXT("rub"));
+    PossibleWords.push_back(TEXT("camp"));
+    PossibleWords.push_back(TEXT("zoo"));
+    PossibleWords.push_back(TEXT("suit"));
+    PossibleWords.push_back(TEXT("squeak"));
+    PossibleWords.push_back(TEXT("drain"));
+    PossibleWords.push_back(TEXT("compare"));
+    PossibleWords.push_back(TEXT("fresh"));
+    PossibleWords.push_back(TEXT("cub"));
+    PossibleWords.push_back(TEXT("hungry"));
+    PossibleWords.push_back(TEXT("injure"));
+    PossibleWords.push_back(TEXT("spiders"));
+    PossibleWords.push_back(TEXT("graceful"));
+    PossibleWords.push_back(TEXT("record"));
+    PossibleWords.push_back(TEXT("turn"));
+    PossibleWords.push_back(TEXT("handsome"));
+    PossibleWords.push_back(TEXT("silky"));
+    PossibleWords.push_back(TEXT("club"));
+    PossibleWords.push_back(TEXT("cup"));
+    PossibleWords.push_back(TEXT("education"));
+    PossibleWords.push_back(TEXT("cheat"));
+    PossibleWords.push_back(TEXT("squalid"));
+    PossibleWords.push_back(TEXT("female"));
+    PossibleWords.push_back(TEXT("jar"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
+    // If resetting the game
+    if (bGameOver)
+    {
+        bGameOver = false;
+        ClearScreen();
+        SetupGame();
+        return;
+    }
 
     /****************************/
     /* CHECK THE PLAYER'S GUESS */
     /****************************/
 
-    PrintLine(HiddenWord);
+    if (bVerbose)
+        PrintLine(FString::Printf(TEXT("The hidden word is %s."), *HiddenWord));
+
+    FString OutputPrompt = TEXT("");
+
     if (Input == HiddenWord)
     {
-        PrintLine(TEXT("You have won!"));
+        OutputPrompt = TEXT("You have won!");
+        GameOver(true);
+        return;
     }
-    else if (!Input.IsEmpty())
+    else if (Input.Len() != HiddenWordLength)
     {
-        PrintLine(TEXT("You have lost..."));
+        // Invalid
+        OutputPrompt = FString::Printf(TEXT("The hidden word is %d characters wrong. Please try again."), HiddenWordLength);
+
+        // Do not remove any lives
+    }
+    else if (!IsIsogram(Input))
+    {
+        // Invalid
+        OutputPrompt = TEXT("The hidden word is an isogram, meaning\nit has no repeating letters.\nPlease try again.");
+
+        // Do not remove any lives
+    }
+    else
+    {
+        // Valid guess, but wrong answer
+        Lives--;
+
+        if (Lives > 0)
+        {
+            OutputPrompt = FString::Printf(TEXT("WRONG. You have %d lives remaining."), Lives);
+        }
+        else
+        {
+            GameOver(false);
+            return;
+        }
     }
 
-    // Check if Input is isogram
-    // Check if Input has correct number of characters
+    PrintLine(OutputPrompt);
+}
 
-    // Remove life
+/********************/
+/* CUSTOM FUNCTIONS */
+/********************/
 
-    // Check if Lives > 0
-    //    If Lives > 0
-    //       Guess again
-    //       Show Lives remaining
-    //    If Lives = 0
-    //       Game Over
-    //       show hidden word
-    //       Prompt to play again --> "Press enter to play again"?
+void UBullCowCartridge::GameOver(bool bWin)
+{
+    bGameOver = true;
+    FString OutputStatement = TEXT("");
+
+    if (bWin)
+    {
+        OutputStatement = FString::Printf(TEXT("Congratulations!\nIt took you %d tries to guess the word."), (HiddenWordLength - Lives + 1));
+    }
+    else
+    {
+        OutputStatement = FString::Printf(TEXT("Game over!\nThe hidden word was %s."), *HiddenWord);
+    }
+    PrintLine(OutputStatement);
+
+    PrintLine("Press Enter to play again or press Tab to leave the terminal.");
+}
+
+bool UBullCowCartridge::IsIsogram(const FString& Guess) const
+{
+    if (Guess.IsEmpty())
+    {
+        PrintLine("Oops. Something went wrong... try again!");
+        return false;
+    }
+
+    TMap<char, bool> LettersSeen = TMap<char, bool>();
+
+    for (int32 index = 0; index < Guess.Len(); index++)
+    {
+        if (LettersSeen.Contains(Guess[index]))
+        {
+            if (LettersSeen[Guess[index]])
+            {
+                // Duplicate letter
+                return false;
+            }
+            else
+            {
+                LettersSeen[Guess[index]] = true;
+            }
+        }
+        else
+        {
+            LettersSeen.Add(Guess[index], true);
+        }
+    }
+
+    return true;
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    auto newWordIndex = rand() % PossibleWords.size();
+    HiddenWord = PossibleWords[newWordIndex];
+    HiddenWordLength = HiddenWord.Len();
+    Lives = HiddenWordLength;
+
+    PrintLine(FString::Printf(TEXT("The hidden word is %d characters long."), HiddenWordLength));
 }
--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Console/Cartridge.h"
+#include <vector>
+using std::vector;
+
 #include "BullCowCartridge.generated.h"
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
@@ -12,11 +15,24 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	GENERATED_BODY()
 
 	public:
+		// UE4 function
 		virtual void BeginPlay() override;
+
+		// Bull Cow pre-determined method
 		virtual void OnInput(const FString& Input) override;
+
+		// Custom functions
+		void GameOver(bool bWin);
+		bool IsIsogram(const FString& Guess) const;
+		void SetupGame();
 
 	// Your declarations go below!
 	private:
-		int32 Lives = 0;
 		FString HiddenWord;
+		int32 HiddenWordLength;
+		int32 Lives = 0;
+		vector<FString> PossibleWords;
+
+		bool bGameOver = false;
+		bool bVerbose = false;
 };
